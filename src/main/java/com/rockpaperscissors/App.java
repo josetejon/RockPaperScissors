95b1109ef50157ce2ec6739b8faa4aca5e6dc4e3
Tejon <jose.tejon@easypaymentgateway.com>
2020-06-15 13:14:03 +0200

    adding last changes

--- src/main/java/com/rockpaperscissors/App.java
+++ src/main/java/com/rockpaperscissors/App.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages="com.rockpaperscissors.controller")
 @ComponentScan(basePackages="com.rockpaperscissors.delegate")
-@ComponentScan(basePackages="com.rockpaperscissors.config")
+@ComponentScan(basePackages="com.rockpaperscissors.filter")
 @ComponentScan(basePackages="com.rockpaperscissors.domain")
 public class App {
 
